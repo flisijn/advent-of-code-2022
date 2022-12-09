8f24c48c7e8e6c6aefbf6a23b6d84d3ba5fea7ae
Hubert Flisijn <h.flisijn@avisi.nl>
2022-12-09 22:05:25 +0100

    Add day template

---
+++ app/src/test/kotlin/adventofcode/common/Day00.kt
@@ -0,0 +1,33 @@
+package adventofcode.common
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day00 {
+    private fun load(path: String) = getResourceAsText(path)
+        .split("\n")
+        .filter { it.isNotBlank() }
+
+
+    private fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    private fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    @Test
+    fun test() {
+        val testData = load("test.txt")
+        assertEquals(0, part1(testData))
+        assertEquals(0, part2(testData))
+    }
+
+    @Test
+    fun run() {
+        val inputData = load("input.txt")
+        println("part 1: ${part1(inputData)}")
+        println("part 2: ${part2(inputData)}")
+    }
+}
